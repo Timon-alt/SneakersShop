28671eba93a42dafe6056378a3a1dd9c05a20639
Timon-alt <vasdaniwe@gmail.com>
2025-02-25 20:27:53 +0500

    testing branch ae

--- app/src/test/java/com/example/sneakersshop/Validator.kt
+++ app/src/test/java/com/example/sneakersshop/Validator.kt
@@ -5,6 +5,9 @@ class Validator {
         return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 
+    // Коммент из ветки sprint-1
+    // Aboba
+
     fun isValidPassword(password: String): Boolean {
         return password.length >= 8
     }
